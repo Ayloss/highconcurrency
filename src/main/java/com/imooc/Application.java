48fc02b57f1bf442f0dd3019b94794ac4ea2abe7
StatusCode200 <358941501@qq.com>
2018-04-12 21:36:28 +0800

    Initial commit

---
+++ src/main/java/com/imooc/Application.java
@@ -0,0 +1,15 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by status200 on 2018/4/12.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
